52a843329af09e8e1f3b056953dee17a1c336a35
Goodguuy98 <75419600+Goodguuy98@users.noreply.github.com>
2022-01-10 19:27:22 +0000

    Added Test Strings

--- Radio Bit.py
+++ Radio Bit.py
@@ -1,3 +1,11 @@
+def on_button_pressed_a():
+    radio.send_string("LigSwiOn")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    radio.send_string("LigSwiOff")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def on_data_received():
     radio.send_string(serial.read_until(serial.delimiters(Delimiters.NEW_LINE)))
 serial.on_data_received(serial.delimiters(Delimiters.NEW_LINE), on_data_received)
