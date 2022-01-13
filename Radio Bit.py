24f81c48a8f87b3959868a603884ae9adb16178f
Goodguuy98 <75419600+Goodguuy98@users.noreply.github.com>
2022-01-13 23:22:46 +0000

    Now recognises claps

--- Radio Bit.py
+++ Radio Bit.py
@@ -2,6 +2,10 @@ def on_button_pressed_a():
     radio.send_string("LigSwiOn")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_received_string(receivedString):
+    serial.write_line(receivedString)
+radio.on_received_string(on_received_string)
+
 def on_button_pressed_b():
     radio.send_string("LigSwiOff")
 input.on_button_pressed(Button.B, on_button_pressed_b)
