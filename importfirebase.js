beb6df172115f3d730d316b80e1993e8e2246e83
Goodguuy98 <ben.mckeever@maristdundalk.ie>
2022-03-20 16:41:20 +0000

    Refinements
    Tab indexes added
    Minus tab index used to prevent tabbing to Youtube video
    
    Higher quality images added.
    
    Comented code
    Added Message and Date verification

--- importfirebase.js
+++ importfirebase.js
@@ -28,7 +28,7 @@ console.log("Firebase imported!")
 //Write Data to firebase and update webpage.
 
 function writeToggleData(dev) {
-    
+
     const toggleCountRef = ref(db, 'embedded/' + dev + '/Swi/');
     
     //Grabs data from firebase once.
@@ -40,7 +40,7 @@ function writeToggleData(dev) {
             //Adds a value to the tally.
             var tallySnap = Object.values(data)[0] + 1
 
-            //Updates firebase with new value
+            //Updates firebase with new value.
             set(ref(db, 'embedded/' + dev + '/Swi'), {
                 toggleNum: tallySnap,
             });
@@ -50,21 +50,19 @@ function writeToggleData(dev) {
     });
 }
 
-function syncToggleData(dev, tallyReq) {
+function syncToggleData(dev) {
 
-    
     const toggleCountRef = ref(db, 'embedded/' + dev + '/Swi/');
 
     //Continuously grabs data from Firebase.
     onValue(toggleCountRef, (toggleNum) => {
 
-    //The data given as an Object.
+    //Data given as an Object.
     const data = toggleNum.val();
 
     //Convert the Object's values to an array and take the first value.
     let tallyCurrent = Object.values(data)[0]
     
-
     //Continously displays latest value.
     document.getElementById("counter").innerHTML = String(tallyCurrent)
     });
@@ -83,17 +81,44 @@ if (document.URL.includes("Control") ) {
 //Control Panel Code--------------------------------------------------------------------
 //Account Form Code--------------------------------------------------------------------
 
-// Grab form html element
+function setMaxDate() {
+    //Set maximum date for birthday to 18 years ago today, to ensure user is 18 years old.
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear() - 18;
+
+    //Format data in congruence with HTML5 max attribute syntax.
+    if (dd < 10) {dd = '0' + dd;}
+    if (mm < 10) {mm = '0' + mm;} 
+    
+    //Create string.
+    today = yyyy + '-' + mm + '-' + dd;
+    //Set max attribute.
+    document.querySelector('input[type="date"]').setAttribute("max", today);
+}
+
+// Grab form html element.
 if (document.URL.includes("Create") ) {
     const form = document.querySelector(".form")
+    setMaxDate()
 }
 
 function captureUserData() {
     var inputs, index;
 
+    //Message must not be more than 100 characters.
+    if (document.querySelector('textarea').value.length > 100 ) {
+        //Notify user
+        document.querySelector(".u-form-send-error").innerHTML = "Your message cannot be more than 100 characters. Please shorten it!"
+        //Cancel data submission
+        return
+    }
+
+    //JSON for data collection and firebase submission
     let loginData = {};
 
-    //Exclude junk via CSS psuedo-class selector.
+    //Exclude junk inputs via CSS psuedo-class selector.
     inputs = document.querySelectorAll('input:not([type="hidden"]):not([type="submit"]), textarea')
 
     for (index = 0; index < inputs.length; ++index) {
@@ -117,6 +142,8 @@ function captureUserData() {
     set(ref(db, 'users/' + userid), loginData);
 
     console.log("Sent to Firebase!")
+
+    document.querySelector(".u-form-send-error").innerHTML = "Thank you! Your message has been sent."
 }
 
 //Listen for a form submission, only on create an account page.
@@ -128,11 +155,11 @@ if (document.URL.includes("Create") ) {
         });
 }
 
-//Control Panel Code--------------------------------------------------------------------
+//Account Form Code--------------------------------------------------------------------
 //Welcome Code--------------------------------------------------------------------------
 
-const pre = ["lovingly written", "imposingly dictated", "hastily telegraphed", "brandishly typed", "poetically recited", "line dropped", "carefully composed", "jubilantly passed on", "concisely conveyed", "needlessly broadcasted", "prolificly proclaimed", "covertly divulged", "happily imparted"]
-const adj = ["doglike", "petulant", "great", "humble", "bodacious", "chucklesome", "eccentric", "amiable", "gregarious", "anti-social", "forlorne", "contested", "impartial"]
+const pre = ["lovingly written", "passionately dictated", "hastily telegraphed", "brandishly typed", "poetically recited", "line dropped", "carefully composed", "jubilantly passed on", "concisely conveyed", "necessarily broadcasted", "prolificly proclaimed", "covertly divulged", "happily imparted"]
+const adj = ["adept", "charming", "great", "humble", "bodacious", "chucklesome", "eccentric", "amiable", "determined", "gregarious", "loyal", "sincere", "impartial"]
 
 function populateQuotes() {
 
@@ -149,21 +176,28 @@ function populateQuotes() {
                 //The current length of object keys array will serve as cap for number generation.
                 let currentLen = Object.keys(data).length
                 //Generate a random number between 0 (inclusive) and amount of users (exclusive).
+                //Random message
                 let randMsg = (Math.floor((Math.random() * currentLen) + 0));
+                //Random preamble
                 let randPre = (Math.floor((Math.random() * pre.length) + 0));
+                //Random adjective
                 let randAdj = (Math.floor((Math.random() * adj.length) + 0));
 
+                //Generates the user's unique introduction
                 let intro = pre[randPre] + " by the " + adj[randAdj] + " "
-
+                //Concatenates the message, introduction and username together.
                 qSlots[i].innerHTML = Object.values(data)[randMsg]['msg'] + " - " + intro +  Object.keys(data)[randMsg]
+                //Deletes the chosen message from the Object to prevent it from recurring
                 delete data[Object.keys(data)[randMsg]]
 
+                //Break the loop if all block quote slots are occupied.
                 if (i == qSlots.length - 1) {
                     break
                 }
 
             }
         } else {
+            //Data does not exist! Nothing can be done.
             return
         }
     });
